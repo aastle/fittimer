f379e52f1c71f585d87e4c2cccb946ff38d7c7b5
Alan Astle <aastle@yahoo.com>
2013-12-15 10:28:16 -0500

    added dialog box which shows date, times and app states returned from sqlite database.

--- fittimer/src/main/java/com/aastle/fittimer/MainActivity.java
+++ fittimer/src/main/java/com/aastle/fittimer/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        BugSenseHandler.initAndStartSession(this, APIKEY);
+        BugSenseHandler.initAndStartSession(getBaseContext(), APIKEY);
         setContentView(R.layout.activity_main);
         throb = checkThrobberPref();
         stopWatch = (StopWatch) findViewById(R.id.stopwatch);
